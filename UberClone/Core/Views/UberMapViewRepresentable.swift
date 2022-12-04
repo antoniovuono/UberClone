afdab20149b1fe027665f1795ac675f3889485e6
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-12-04 12:25:31 -0300

    create map

--- UberClone/Core/Views/UberMapViewRepresentable.swift
+++ UberClone/Core/Views/UberMapViewRepresentable.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import SwiftUI
+import MapKit
+
+
+struct UberMapViewRepresentable: UIViewRepresentable {
+    
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MaapCoordinator {
+        return MaapCoordinator(parent: self)
+    }
+}
+
+
+extension UberMapViewRepresentable {
+    class MaapCoordinator: NSObject, MKMapViewDelegate {
+        let parent: UberMapViewRepresentable
+        
+        init(parent: UberMapViewRepresentable) {
+            self.parent = parent
+            super.init()
+        }
+    }
+}
+
