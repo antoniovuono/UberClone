6c4630d6de1f2b4794ef206092b77825baad5489
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-12-04 11:43:40 -0300

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Antonio Vuono on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
